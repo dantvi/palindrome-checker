5e48b23f35b152effb006e677dbf934c58e62f50
dantvi <daniel.tving@gmail.com>
2024-03-17 18:41:18 +0100

    change input text and compare

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 const textInputEl = document.getElementById('text-input');
 const btn = document.getElementById('check-btn');
-let textInputValue = ';'
+let resultTextEl = document.getElementById('result-text');
+let textInputValue = '';
 let convertedText = '';
+let reversedText = '';
 
 function convertText(inputString) {
-    // Convert to Lowercase String
+    // Convert to Lowercase
     let lowerCaseString = inputString.toLowerCase();
     // Remove All Non-Alphanumeric Characters 
     return lowerCaseString.replace(/[^a-zA-Z0-9]/g, '');
 };
 
+function reverseText(str){ 
+    const reversedString =  
+    str.split("").reduce((acc, char) => char + acc, ""); 
+    return reversedString;
+} 
+
 function compareText() {
     textInputValue = textInputEl.value;
-    // Check If Text Input is empty
+    // Check If Text Input is Empty
     if (textInputValue === '') {
         alert('Please input a value');
     } else {
-        console.log('before remove:', textInputValue);
-        console.log('after remove:', convertText(textInputValue));
+        // Change Input String
+        convertedText = convertText(textInputValue);
+        reversedText = reverseText(convertedText);
+        if (convertedText === reversedText) {
+            resultTextEl.textContent = `${textInputValue} is a palindrome`
+        } else {
+            resultTextEl.textContent = `${textInputValue} is not a palindrome`
+        }
     }
 };
 
 // Event Listeners
 btn.addEventListener('click', compareText);
-
-
-/* Pseudokod 
-Ta bort alla icke alfabetiska characters ex. punkter, mellanrum och symboler. 
-Konvertera hela texten till lower case
-Använd en metod som vänder på textInput och sparar in i en variabel, convertedText. 
-Jämför textInput med convertedText
-
-Om de är lika retunera: `${textInput} is a palindrome`
-Annars retunera: `${textInput} is not a palindrome`
-
-Om skriver in eye och klickar på knappen ska texten: "eye is a palindrome" skrivas ut.
-Om skriver in nope och klickar på knappen ska texten: "nope is not a palindrome" skrivas ut.
-*/
